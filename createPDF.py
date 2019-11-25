ac36f669c41e4102dc8603456971bac8835cfd81
loris <loris.schmit@yahoo.com>
2019-11-25 17:21:25 +0100

    monthly balance sheet added

--- createPDF.py
+++ createPDF.py
@@ -1,7 +1,14 @@
 from reportlab.pdfgen import canvas
-import os
-from pdfrw import pdfwriter
-from fourUp import toFourUp
+from reportlab.lib import colors
+from operator import itemgetter
+
+from commonFunctions import perWeek,readCSVtoObjectExpense,monthNumberToMonthName,total,perTag
+from reportlab.graphics import renderPDF
+from svglib.svglib import svg2rlg
+
+from reportlab.platypus import Table, TableStyle
+
+from budget import getBudgetPerYear,getBudgetPerMonth
 
 
 def drawMyRuler(pdf):
@@ -24,30 +31,149 @@ def drawMyRuler(pdf):
     pdf.drawString(10, 800, 'y800')
 
 
-
-def drawPDF():
-
-    file_name = "test.pdf"
-    document_title = "This is the title"
-    title = "This is the real title"
-    text = ["This pdf is just a dummy pdf to test if","the module even works"]
+def drawPDF(file,month,year):
+    file_name = "Balance Sheets/"+str(month+1)+"-"+file+".pdf"
+    image_path = "images/"+file+".svg"
+    document_title = file
+    title = file
+    transacts = readCSVtoObjectExpense(month, year)
+    total_spent = total(transacts)
 
     pdf = canvas.Canvas(file_name)
+    #drawMyRuler(pdf)
 
     pdf.setTitle(document_title)
 
-    drawMyRuler(pdf)
+    drawImage(image_path,pdf)
+
+    pdf.setFont("Helvetica", 18)
+
+    tags = perTag(transacts)
+    pdf.drawString(350,720,'Ausgaben pro Kategorie')
+    pdf.line(350,718,546,718)
+    t_category = drawCategoryTable(tags)
+    t_category.wrapOn(pdf,500,300)
+    t_category.drawOn(pdf,400,700-len(tags)*25)
+
+    pdf.setFont("Helvetica-Bold", 30)
+    pdf.drawCentredString(300, 780, title)
+
+    pdf.setFont("Helvetica", 18)
+
+
+    weeks = perWeek(transacts)
+    pdf.drawString(50, 320, 'Ausgaben pro Woche')
+    pdf.line(50,318,225,318)
+    t_weeks = drawWeeksTable(weeks,8)
+    t_weeks.wrapOn(pdf,500, 300)
+    t_weeks.drawOn(pdf,50, 250)
+
+    pdf.line(50, 220, 540, 220)
+
+
+    pdf.drawString(50,185,'Bilanz')
+    pdf.line(50, 183, 100, 183)
+    budget = getBudgetPerMonth(getBudgetPerYear(2018))
+    t_balance = drawBalanceTable(budget,total_spent)
+    t_balance.wrapOn(pdf,500,300)
+    t_balance.drawOn(pdf,50,95)
+
+    pdf.setFont("Helvetica-Bold", 22)
+    pdf.drawString(50,50,"Gesamtausgaben: "+str(total_spent)+" €")
+
 
     pdf.save()
 
 
-def getGraphs():
-    dir = 'images'
-    entries = os.listdir(dir)
-    path = dir+"/"+"Januar 2019.pdf"
-    toFourUp(path)
+def drawImage(image_path,pdf):
+    drawing = svg2rlg(image_path)
+    #drawing.renderScale = 0.001
+    renderPDF.draw(drawing, pdf,  -150, 350)
+
+def drawWeeksTable(weeks,month):
+    data = []
+    week_dates =[]
+    for week in weeks.keys():
+        start = week[0].strftime("%d/%m")
+        end = week[1].strftime("%d/%m")
+        if week[0].month == week[1].month:
+            week_dates.append(start+" - "+end)
+        elif week[0].month < month:
+            week_dates.append("     - " + end)
+        elif week[1].month > month:
+            week_dates.append(start + " -     ")
+    data.append(reversed(week_dates))
+    week_values = list(reversed(list(weeks.values())))
+    week_values_str = []
+    for value in week_values:
+        week_values_str.append(str(value)+" €")
+    data.append(week_values_str)
+    rowHeights = len(data)*[25]
+    t=Table(data,rowHeights=rowHeights)
+    t.setStyle(TableStyle([('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                           ('ALIGN',(0,0),(-1,-1),'CENTER'),
+                           ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+                           ('FONTSIZE', (0, 0), (-1, -1), 15),
+                           ]))
+    return t
+
+def drawCategoryTable(tags):
+    data = []
+    for key in tags.keys():
+        data.append([key,tags[key]])
+    rowHeights = len(data) * [25]
+    data = list(reversed(sorted(data, key=itemgetter(1))))
+    for index,element in enumerate(data):
+        value = str(element[1])+" €"
+        data[index][1] = value
+    t = Table(data, rowHeights=rowHeights)
+    t.setStyle(TableStyle([('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                           ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                           ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+                           ('FONTSIZE', (0, 0), (-1, -1), 15),
+                           ]))
+    return t
+
+def drawBalanceTable(budget,spent):
+    data = [['Gesamtausgaben',str(spent)+" €"],
+            ['Budget pro Monat', str(budget)+" €"]]
+    balance = round(budget-spent,2)
+
+    style =[('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                           ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                           ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+                           ('FONTSIZE', (0, 0), (-1, -1), 15),
+                           ]
+    if balance > 0 :
+        balance_str = "Suffizit"
+        style.append(['BACKGROUND', (0, 2), (1, 2), colors.lightgreen])
+    else:
+        balance_str = "Defizit"
+        style.append(['BACKGROUND', (0, 2), (1, 2), colors.rgb2cmyk(255, 150, 110)])
+
+    data.append([balance_str,str(balance)+" €"])
 
 
-getGraphs()
+    rowHeights = len(data) * [25]
+    t = Table(data,rowHeights=rowHeights)
+    t.setStyle(TableStyle(style))
+    return t
 
+
+
+def drawPDFCollection():
+    for i in range(0,9):
+        file = monthNumberToMonthName(i)+" 2019"
+        print(file)
+        drawPDF(file,i,"2019")
+
+
+def main():
+    #drawPDF("Januar 2019",0,"2019")
+    drawPDFCollection()
+    #test()
+    transacts = readCSVtoObjectExpense(7, "2019")
+    #drawCategoryTable(transacts)
+if __name__ == "__main__":
+    main()
 
--- visualizeit.py
+++ visualizeit.py
@@ -2,53 +2,10 @@ import csv
 from Transaction import Transaction
 import plotly.graph_objects as go
 import chart_studio
+from commonFunctions import total,readCSVtoObject,perTag
 #!
 
-def getTransacts(month,year):
-    file_name = year + "/" + str(month+1) + ".csv"
-    transacts = []
-    with open(file_name) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        for row in csv_reader:
-            transacts.append(Transaction(row[0],row[1],row[2],row[3],-float(row[4]),row[5],row[6]))
-        csv_file.close()
-    return transacts
 
-def total(transacts):
-    tot = 0
-    for action in transacts:
-        if action.tag != "income":
-            tot += action.amount
-    return round(tot,2)
-
-def perTag(transacts):
-    tags_temp = {}
-    tags = {}
-    truth_table = []
-    tot = 0
-    tag = ""
-    for el in transacts:
-        truth_table.append(True)
-    for i in range(0,len(transacts)):
-        if truth_table[i]:
-            tot = transacts[i].amount
-            tag = transacts[i].tag
-            for j in range(i+1,len(transacts)):
-                if transacts[i].tag == transacts[j].tag and truth_table[j]:
-                    tot += transacts[j].amount
-                    truth_table[j] = False
-            tot = round(tot, 2)
-            tags_temp[tag] = tot
-
-    rest = 0
-    for tag in tags_temp:
-        if tags_temp[tag] >= 20:
-            tags[tag] = tags_temp[tag]
-        elif tags_temp[tag] > 0:
-            rest += float(tags_temp[tag])
-    rest = round(rest, 2)
-    tags['rest'] = rest
-    return tags
 
 def biggestTag(tags):
     max_key = next(iter(tags))
@@ -60,29 +17,18 @@ def biggestTag(tags):
     return (max_key,max)
 
 
-
-
-def perMonth(transacts):
-    tot = 0
-    for action in transacts:
-        if action.tag != "income":
-            tot += float(action.amount)
-    tot = round(tot,2)
-    return tot
-
-
 def showTags():
     for i in range(1,10):
         month = str(i)
         transacts = getTransacts(month,"2019")
-        print(month+" : "+str(perMonth(transacts)))
+        print(month+" : "+str(total(transacts)))
         tags = perTag(transacts)
         print(tags)
         print(biggestTag(tags))
 
 def createGraphCollection(months,year):
     for month_index, month in enumerate(months):
-        transacts = getTransacts(month_index, year)
+        transacts = readCSVtoObject(month_index, year)
         tags = perTag(transacts)
         tot = total(transacts)
         max = biggestTag(tags)[1]
@@ -101,35 +47,20 @@ def createGraph(data,month_index,year,tot,max):
     if rot_fact<0:
         rot_fact = 0
     title = months[month_index]+" "+year
-    layout = dict(showlegend=False, width=400, height=515, margin=dict(l=5,t=0,r=10,b=0),
+    layout = dict(showlegend=False,
                   font=dict(
                       size=19
                   ),
-                  annotations=[
-                      go.layout.Annotation(
-                          x=1,
-                          y=0.97,
-                          xref="paper",
-                          yref="paper",
-                          text="Total : " + str(tot) + " €",
-                          font=dict(
-                              color="black",
-                              size=20
-                          ),
-                          showarrow=False,
-                      )
-                    ],
                   )
     fig = go.Figure(data=[go.Pie(labels=labels, values=values)],layout=layout)
     fig.update_traces(textinfo='label',hoverinfo='percent+value',rotation=rot_fact,)
     #fig.show()
-    fig.write_image("images/"+title+".pdf")
+    fig.write_image("images/"+title+".svg")
 
 def main():
-    months = ["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober"]
     year = "2019"
     tag_collection = []
-    transacts = getTransacts(7,year)
+    transacts = readCSVtoObject(7,year)
     tags = perTag(transacts)
     tot = total(transacts)
     #embedGraph()
@@ -138,4 +69,5 @@ def main():
     createGraphCollection(months,year)
 
 if __name__ == "__main__":
+    months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober"]
     main()
